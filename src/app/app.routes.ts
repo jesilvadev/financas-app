b0f143f1a96c84387615d0069d1d070b8ed6a66b
Jefferson Silva <jefessh@outlook.com>
2025-11-05 21:23:16 -0300

    feat: adiciona componentes de Stats, History e Profile, e atualiza rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { StatsComponent } from './pages/stats/stats.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    children: [{ path: '', component: HomeComponent }],
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'stats', component: StatsComponent },
+      { path: 'history', component: HistoryComponent },
+      { path: 'profile', component: ProfileComponent },
+    ],
   },
 ];
---
+++ src/app/pages/stats/stats.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stats',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stats.component.html',
+})
+export class StatsComponent {}
